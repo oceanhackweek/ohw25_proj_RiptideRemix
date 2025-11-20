4e8635069559da1cafe246d1ec9bfecd2ccc3126
KaseyMCastello <kcastello@ucsd.edu>
2025-11-20 01:21:49 -0800

    Start Learn

--- riptideremix/views.py
+++ riptideremix/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "riptideremix/riptideremix.html"
+
+class LearnView(TemplateView):
+    template_name = "learn/learn_base.html"
