e89ca84f89879b0ee4d36591153d1c92063721ea
isabrand <imcbrandicourt@gmail.com>
2025-12-18 15:38:01 -0800

    Added spectrogram viewing!

--- mixer/urls.py
+++ mixer/urls.py
@@ -1,6 +1,10 @@
+# mixer/urls.py
 from django.urls import path
-from .views import MixerView
+from .views import MixerView, generate_spectrogram_view
 
 urlpatterns = [
     path('', MixerView.as_view(), name='mixer'),
+    path('spectrogram/', generate_spectrogram_view, name='generate_spectrogram'),
 ]
+
+print("DEBUG: URLs loaded successfully")
