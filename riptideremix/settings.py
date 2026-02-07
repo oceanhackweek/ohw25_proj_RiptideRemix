350db83b4af266ec1b07c4c831f1fab70f82b024
KaseyMCastello <kcastello@ucsd.edu>
2026-02-06 21:15:50 -0800

    render prepare2

--- riptideremix/settings.py
+++ riptideremix/settings.py
@@ -85,14 +85,17 @@ WSGI_APPLICATION = 'riptideremix.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+# Only require SSL for non-SQLite databases
+if DATABASE_URL.startswith("sqlite"):
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
 
 
 # Password validation
